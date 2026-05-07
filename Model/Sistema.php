4393111f18385dcd788c5162e7d6fc8d421a8016
Matheus <dominatomatheus06@gmail.com>
2026-05-07 11:30:44 -0300

    Tabela p/ o banco feita

---
+++ Model/Sistema.php
@@ -0,0 +1,23 @@
+<?php
+Class Sistema {
+    private $conn;
+
+    public function __construct(){
+        $database = new Database();
+        $this->conn = $database->getConnetion();
+
+    }
+
+    public function mostrarTabela(){
+        $query =
+    }
+    
+}
+
+
+
+
+
+
+
+?>
