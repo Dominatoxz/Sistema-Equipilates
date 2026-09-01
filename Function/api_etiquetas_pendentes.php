f622117eb23ec1b77a8a607c2abe3ae6d31a8045
Matheus Dominato <dominatomatheus06@gmail.com>
2026-09-01 10:56:20 -0300

    Aumenta largura do codigo de barras (BY3 -> BY5)

--- Function/api_etiquetas_pendentes.php
+++ Function/api_etiquetas_pendentes.php
@@ -86,7 +86,7 @@ function montarZpl(array $item, string $tipo, bool $misto): string
     // destaque, sub-linha peça/prazo, cor, e código de barras no rodapé.
     $ehEmbalagem  = $tipo === 'EMBALAGEM';
     $tituloTipo   = $ehEmbalagem ? 'EMBALAGEM' : 'PRODUCAO';
-    $larguraBarra = $ehEmbalagem ? 100 : 14;
+    $larguraBarra = $ehEmbalagem ? 50 : 14;
     $baseX        = $larguraBarra + 24;
     $larguraUtil  = 807 - $baseX - 20;
 
@@ -112,7 +112,7 @@ function montarZpl(array $item, string $tipo, bool $misto): string
     $zpl .= "^FO{$baseX},148^A0N,22,22^FD{$subLinha}^FS\n";
     $zpl .= "^FO{$baseX},176^A0N,22,22^FD{$corLinha}^FS\n";
     $zpl .= "^FO{$baseX},204^A0N,22,22^FB{$larguraUtil},1,0,C^FDID: {$codigo}^FS\n";
-    $zpl .= "^BY3\n^FO" . ($baseX + 40) . ",232^BCN,90,N,N,N^FD{$codigo}^FS\n";
+    $zpl .= "^BY5\n^FO" . ($baseX + 20) . ",232^BCN,90,N,N,N^FD{$codigo}^FS\n";
     $zpl .= "^XZ\n";
 
     return $zpl;
